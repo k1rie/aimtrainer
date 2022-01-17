d9c0c4c861ebd12d317be455474754456e5a3958
k1rie <d628587@gmail.com>
2022-01-16 19:53:14 -0600

    update1

--- index.js
+++ index.js
@@ -1,37 +1,151 @@
 const score = document.querySelector("#score")
 
+const pausebtn = document.querySelector("#pausebtn")
+
+const totaltargets = document.querySelector("#totaltargets")
+
+const select = document.querySelector("#dificulty")
+
+const quiticon = document.querySelector("#quiticon")
+
+const menu = document.querySelector("#menu")
+
+const settings = document.querySelector("#settings")
+
+const save = document.querySelector("#savebtn")
+
+const targeturl = document.querySelector("#targeturl")
+
+const backgroundurl = document.querySelector("#backgroundurl")
+
 var screenwidth = screen.width
 
 var screenheight = screen.height
 
 var scorecount = 0
 
+var targetscount = 0
 
+const easy = 2000
 
+const medium = 1300
 
+const hard = 700
 
-setInterval(()=>{
-    const target = document.createElement("div")
-    target.style.position = "fixed"
-    target.style.backgroundColor = "black"
-    target.style.width = "50px"
-    target.style.height = "50px"
-    target.style.borderRadius = "100%"
-    target.style.top = `${Math.floor(Math.random() * screenheight)}px`
-    target.style.left = `${Math.floor(Math.random() * screenwidth)}px`
-    target.style.transition = "1s all"
-    document.querySelector("body").append(target)
-    setTimeout(()=>{
-        target.style.width = "0px"
-        target.style.height = "0px"
-    },1000)
-target.addEventListener("click",(e)=>{
-target.style.display = "none"
-scorecount++
-score.textContent = `score:${scorecount}`
+quiticon.addEventListener("click",()=>{
+menu.style.display = "none"
 })
-},400)
+
+settings.addEventListener("click",()=>{
+    menu.style.display = "flex"
+})
+
+save.addEventListener("click",()=>{
+    localStorage.setItem("targeturl",targeturl.value)
+    localStorage.setItem("backgroundurl",backgroundurl.value)
+})
 
 
 
 
+
+
+
+pausebtn.addEventListener("click",()=>{
+    if(pausebtn.checked === true){
+        scorecount = 0
+        totaltargets.textContent = `Total Targets:${targetscount}`
+    }
+ 
+        if(select.value === "easy"){
+        const gentarget = setInterval(()=>{
+           if(pausebtn.checked === false && select.value === "easy"){
+                const target = document.createElement("img")
+            target.style.position = "fixed"
+            target.src = localStorage.getItem("targeturl")
+            target.style.backgroundColor = "black"
+            target.style.objectFit = "cover"
+            target.style.width = "50px"
+            target.style.height = "50px"
+            target.style.borderRadius = "100%"
+            target.style.top = `${Math.floor(Math.random() * screenheight)}px`
+            target.style.left = `${Math.floor(Math.random() * screenwidth)}px`
+            target.style.transition = "1s all"
+            document.querySelector("body").append(target)
+            setTimeout(()=>{
+                target.style.width = "0px"
+                target.style.height = "0px"
+            },1000)
+        target.addEventListener("click",(e)=>{
+        target.style.display = "none"
+        scorecount++
+        score.textContent = `score:${scorecount}`
+        })
+            
+          targetscount++  
+    }
+        },easy)
+    }
+
+    if(select.value === "medium"){
+        const gentarget = setInterval(()=>{
+            if(pausebtn.checked === false && select.value === "medium"){
+                const target = document.createElement("img")
+            target.style.position = "fixed"
+            target.src = localStorage.getItem("targeturl")
+            target.style.backgroundColor = "black"
+            target.style.objectFit = "cover"
+            target.style.width = "50px"
+            target.style.height = "50px"
+            target.style.borderRadius = "100%"
+            target.style.top = `${Math.floor(Math.random() * screenheight)}px`
+            target.style.left = `${Math.floor(Math.random() * screenwidth)}px`
+            target.style.transition = "1s all"
+            document.querySelector("body").append(target)
+            setTimeout(()=>{
+                target.style.width = "0px"
+                target.style.height = "0px"
+            },1000)
+        target.addEventListener("click",(e)=>{
+        target.style.display = "none"
+        scorecount++
+        score.textContent = `score:${scorecount}`
+        })
+            
+          targetscount++ 
+    } 
+        },medium)
+    }
+
+    if(select.value === "hard"){
+        const gentarget = setInterval(()=>{
+            if(pausebtn.checked === false && select.value === "hard"){
+                const target = document.createElement("img")
+            target.style.position = "fixed"
+            target.src = localStorage.getItem("targeturl")
+            target.style.backgroundColor = "black"
+            target.style.objectFit = "cover"
+            target.style.width = "50px"
+            target.style.height = "50px"
+            target.style.borderRadius = "100%"
+            target.style.top = `${Math.floor(Math.random() * screenheight)}px`
+            target.style.left = `${Math.floor(Math.random() * screenwidth)}px`
+            target.style.transition = "1s all"
+            document.querySelector("body").append(target)
+            setTimeout(()=>{
+                target.style.width = "0px"
+                target.style.height = "0px"
+            },1000)
+        target.addEventListener("click",(e)=>{
+        target.style.display = "none"
+        scorecount++
+        score.textContent = `score:${scorecount}`
+        })
+            
+          targetscount++  
+    }
+        },hard)
+    }
+
+    console.log(select.value)
+})
